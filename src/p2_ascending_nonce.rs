4fdc65198254e49a18d82f9a2a5b3d6ebc26c0c5
tobiasruck <ruck.tobias@gmail.com>
2019-10-13 16:32:02 +0200

    Implemented refilling+queryable pk

--- src/p2_ascending_nonce.rs
+++ src/p2_ascending_nonce.rs
@@ -17,10 +17,13 @@ pub struct P2AscendingNonce {
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum P2AscendingNonceSpendParams {
-    Nonce {
+    NonceRedeem {
         payment_amount: i32,
         new_nonce: i32,
         owner_sig: Vec<u8>,
+    },
+    NonceRefill {
+        payment_amount: i32,
     },
     P2pk,
 }
@@ -37,92 +40,134 @@ impl P2AscendingNonce {
             ].concat().to_vec()),
             Push(self.owner_pk.clone()),
             Code(OpRot),
-            Code(OpIf),
         ];
-        // case Nonce
-        ops.append(&mut vec![
-            Code(OpToAltStack),
-            Code(OpBin2Num),
-            Code(OpOver),
-            Code(OpLessThan),
-            Code(OpVerify),
-            Push(vec![8]),  // (=nonce size)
-            Code(OpNum2Bin),
-            Code(OpDup),
-            Code(OpToAltStack),
-            Push(vec![0x08]), // (=PUSH 8 bytes)
-            Code(OpSwap),
-            Code(OpCat),
-            Code(OpOver),
-            Code(OpCat),
-            Code(OpHash160),
-            Code(OpSwap),
-            Code(OpToAltStack),
-            Code(OpToAltStack),
-            Code(Op2Dup),
-            Code(OpSwap),
-            Code(OpSub),
-            Push(vec![8]),  // (=value size)
-            Code(OpNum2Bin),
-            Push(vec![23, OpHash160 as u8, 20]),  // (=p2shpre)
-            Code(OpFromAltStack),
-            Push(vec![OpEqual as u8]),
-            Code(OpCat),
-            Code(OpCat),
-            Code(OpCat),
-            Push(encode_int(7)),  // <outputspost>
-            Code(OpPick),
-            Code(OpCat),
-            Code(OpHash256),
-            Code(OpSwap),
-            Push(vec![8]),  // (=value size)
-            Code(OpNum2Bin),
-            Push(encode_int(4)),  // <preimageprefix>
-            Code(OpRoll),
-            Code(OpSize),
-            Push(encode_int(4 + 32 + 32 + (32 + 4) + 1 + 9)),  // (=preimage prefix size)
-            Code(OpNumEqualVerify),
-            Code(OpFromAltStack),
-            Code(OpCat),
-            Code(OpSwap),
-            Code(OpCat),
-            Push(b"\xff\xff\xff\xff".to_vec()), // (=seq no)
-            Code(OpCat),
-            Code(OpSwap),
-            Code(OpCat),
-            Code(OpRot),
-            Code(OpSize),
-            Push(vec![8]),  // (=preimage suffix size)
-            Code(OpNumEqualVerify),
-            Code(OpCat),
-            Code(OpSha256),
-            Code(Op2Swap),
-            Code(Op2Dup),
-            Push(vec![0x41]),  //  (=sighash_all)
-            Code(OpCat),
-            Code(OpSwap),
-            Code(OpCheckSigVerify),
-            Code(OpRot),
-            Code(OpRot),
-            Code(OpCheckDataSigVerify),
-            Push(vec![8]),  // (=payment amount size)
-            Code(OpNum2Bin),
-            Code(OpFromAltStack),
-            Code(OpCat),
-            Code(OpSwap),
-            Code(OpSha256),
-            Code(OpSwap),
-            Code(OpCat),
-            Code(OpFromAltStack),
-            Code(OpCheckDataSigVerify),
-            Push(self.lokad_id.clone()),
-            Code(OpEqual),
-        ]);
+        ops.push(Code(OpIf));
+        {
+            // case Nonce
+            ops.append(&mut vec![
+                Code(OpToAltStack),
+                Code(OpBin2Num),
+                Code(OpOver),
+                Push(encode_int(6)),  // (=paymentAmount)
+                Code(OpPick),
+                Push(vec![]),
+                Code(OpGreaterThanOrEqual),
+            ]);
+            ops.push(Code(OpIf));
+            {
+                // case: redeem
+                ops.push(Code(OpLessThan));
+            }
+            ops.push(Code(OpElse));
+            {
+                // case: refill
+                ops.push(Code(OpEqual));
+            }
+            ops.push(Code(OpEndIf));
+            ops.append(&mut vec![
+                Code(OpVerify),
+                Push(vec![8]),  // (=nonce size)
+                Code(OpNum2Bin),
+                Code(OpDup),
+                Code(OpToAltStack),
+                Push(vec![0x08]), // (=PUSH 8 bytes)
+                Code(OpSwap),
+                Code(OpCat),
+                Code(OpOver),
+                Code(OpCat),
+                Code(OpHash160),
+                Code(OpRot),
+                Code(OpRot),
+                Code(OpCat),
+                Code(OpToAltStack),
+                Code(OpToAltStack),
+                Code(Op2Dup),
+                Code(OpSwap),
+                Code(OpSub),
+                Push(vec![8]),  // (=value size)
+                Code(OpNum2Bin),
+                Push(vec![23, OpHash160 as u8, 20]),  // (=p2shpre)
+                Code(OpFromAltStack),
+                Push(vec![OpEqual as u8]),
+                Code(OpCat),
+                Code(OpCat),
+                Code(OpCat),
+                Push(encode_int(7)),  // <outputspost>
+                Code(OpRoll),
+                Code(OpCat),
+                Code(OpHash256),
+                Code(OpSwap),
+                Push(vec![8]),  // (=value size)
+                Code(OpNum2Bin),
+                Push(encode_int(4)),  // <preimageprefix>
+                Code(OpRoll),
+                Code(OpSize),
+                Push(encode_int(4 + 32 + 32 + (32 + 4) + 1 + 9)),  // (=preimage prefix size)
+                Code(OpNumEqualVerify),
+                Code(OpFromAltStack),
+                Code(OpCat),
+                Code(OpSwap),
+                Code(OpCat),
+                Push(b"\xff\xff\xff\xff".to_vec()), // (=seq no)
+                Code(OpCat),
+                Code(OpSwap),
+                Code(OpCat),
+                Code(OpRot),
+                Code(OpSize),
+                Push(vec![8]),  // (=preimage suffix size)
+                Code(OpNumEqualVerify),
+                Code(OpCat),
+                Code(OpSha256),
+                Code(Op2Swap),
+                Code(OpOver),
+                Code(OpToAltStack),
+                Code(Op2Dup),
+                Push(vec![0x41]),  //  (=sighash_all)
+                Code(OpCat),
+                Code(OpSwap),
+                Code(OpCheckSigVerify),
+                Code(OpRot),
+                Code(OpRot),
+                Code(OpCheckDataSigVerify),
+                Code(OpDup),
+                Push(vec![]),
+                Code(OpGreaterThanOrEqual),
+            ]);
+            ops.push(Code(OpIf));
+            {
+                // case: redeeming
+                ops.append(&mut vec![
+                    Push(vec![8]),  // (=payment amount size)
+                    Code(OpNum2Bin),
+                    Code(OpFromAltStack),
+                    Code(OpHash160),
+                    Code(OpSwap),
+                    Code(OpCat),
+                    Code(OpFromAltStack),
+                    Code(OpCat),
+                    Code(OpFromAltStack),
+                    Code(OpCheckDataSigVerify),
+                    Push(self.lokad_id.clone()),
+                    Code(OpEqual),
+                ]);
+            }
+            ops.push(Code(OpElse));
+            {
+                // case: refilling
+                ops.append(&mut vec![
+                    Code(Op2Drop),
+                ]);
+            }
+            ops.push(Code(OpEndIf));
+        }
         ops.push(Code(OpElse));
-        ops.append(&mut vec![
-            Code(OpNip),
-            Code(OpCheckSig),
-        ]);
+        {
+            // case p2pk
+            ops.append(&mut vec![
+                Code(OpNip),
+                Code(OpCheckSig),
+            ]);
+        }
         ops.push(Code(OpEndIf));
 
         ops
@@ -148,11 +193,22 @@ impl Output for P2AscendingNonce {
                   pre_image: &PreImage,
                   outputs: &[TxOutput]) -> Script {
         use self::P2AscendingNonceSpendParams::*;
-        match self.spend_params.as_ref().expect("must provide spend params!") {
-            Nonce { payment_amount, new_nonce, owner_sig } => {
+        let spend_params = self.spend_params.as_ref().expect("must provide spend params!");
+        match spend_params {
+            NonceRedeem { .. } | NonceRefill { .. } => {
+                let (payment_amount, new_nonce, owner_sig) = match spend_params {
+                    NonceRedeem { payment_amount, new_nonce, owner_sig } => {
+                        (*payment_amount, *new_nonce, owner_sig.clone())
+                    },
+                    NonceRefill { payment_amount } => {
+                        (*payment_amount, self.old_nonce, vec![])
+                    },
+                    P2pk => unreachable!(),
+                };
                 serialized_sig.remove(serialized_sig.len() - 1);  // remove sig flag
                 let script_code = self.script_code().to_vec_sig();
-                let nonce_cutoff = 9;  // len("PUSH <oldNonce>")
+                let nonce_size = 9;  // len("PUSH <oldNonce>")
+                let pk_size = 34;  // len("PUSH <pubkey>")
                 Script::new(vec![
                     Op::Push(self.lokad_id.clone()),
                     Op::Push(owner_sig.clone()),  // ownerDataSig
@@ -181,7 +237,7 @@ impl Output for P2AscendingNonce {
                             sighash_type: false,
                         }).unwrap();
                         write_var_int(&mut pre_image_part, script_code.len() as u64).unwrap();
-                        pre_image_part.extend_from_slice(&script_code[..nonce_cutoff]);
+                        pre_image_part.extend_from_slice(&script_code[..nonce_size]);
                         pre_image_part
                     }),
                     Op::Push({  // preimageSuffix
@@ -200,10 +256,11 @@ impl Output for P2AscendingNonce {
                         }).unwrap();
                         pre_image_part
                     }),
-                    Op::Push(encode_int(*payment_amount)),
+                    Op::Push(encode_int(payment_amount)),
                     Op::Push(encode_int(self.old_value.try_into().unwrap())),
-                    Op::Push(script_code[nonce_cutoff..].to_vec()),
-                    Op::Push(encode_int(*new_nonce)),
+                    Op::Push(script_code[nonce_size..][..pk_size].to_vec()),
+                    Op::Push(script_code[nonce_size..][pk_size..].to_vec()),
+                    Op::Push(encode_int(new_nonce)),
                     Op::Push(vec![1]),
                 ])
             },
